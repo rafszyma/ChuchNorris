7ca27eeed338b2d11dfd047e1e81c522f672410d
rafal.szymala <rafalaszymala@gmail.com>
2022-09-18 21:13:59 +0200

    test headers for harvester

--- Tests/ApiTests.cs
+++ Tests/ApiTests.cs
@@ -5,18 +5,15 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using API;
-using Bogus;
 using Business.Persistence;
 using FluentAssertions;
-using Newtonsoft.Json;
 using Tests.Infrastructure;
 using Xunit;
 
 namespace Tests;
 
-public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
+public class ApiTests : IClassFixture<ChuckWebAppFactory>, IAsyncLifetime
 {
-
     private readonly HttpClient _client;
 
     public ApiTests(ChuckWebAppFactory factory)
@@ -29,10 +26,10 @@ public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
     {
         var response = await _client.GetStringAsync($"/user/{UserRoles.Admin}");
         response.Length.Should().BeGreaterThan(10);
-        
+
         // TODO check if its valid JWT token
     }
-    
+
     [Fact]
     public async Task Given_InvalidRole_When_GettingToken_Then_BadRequestIsReturned()
     {
@@ -51,14 +48,14 @@ public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
         response!.Length.Should().Be(15);
         response.Should().BeEquivalentTo(quotes);
     }
-    
+
     [Fact]
     public async Task Given_NoQuotesInDatabase_When_ListingQuotes_Then_EmptyListIsReturned()
     {
         var response = await _client.GetFromJsonAsync<Quote[]>("/quote/top");
         response.Should().BeEmpty();
     }
-    
+
     [Fact]
     public async Task Given_QuotesWithDifferentScore_When_GettingTopQuotes_Then_OnlyTopQuotesAreReturned()
     {
@@ -66,26 +63,26 @@ public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
         await TestData.InsertRecords(quotes.ToArray());
 
         var topQuotes = quotes.OrderByDescending(x => x.Score).Take(10);
-        
+
         var response = await _client.GetFromJsonAsync<Quote[]>("/quote/top");
         response.Should().NotBeNull();
         response!.Length.Should().Be(10);
         response.Should().BeEquivalentTo(topQuotes);
     }
-    
+
     [Fact]
     public async Task Given_NoQuotesInDatabase_When_GettingTopQuotes_Then_EmptyListIsReturned()
     {
         var response = await _client.GetFromJsonAsync<Quote[]>("/quote/top");
         response.Should().BeEmpty();
     }
-    
+
     [Fact]
     public async Task Given_ExistingId_When_VotingForQuote_Then_ScoreIsIncreased()
     {
         var quote = new QuoteFaker().Generate();
         await TestData.InsertRecords(quote);
-        
+
         var response = await _client.PutAsync($"/quote/{quote.Id}/vote", null);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
@@ -96,21 +93,21 @@ public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
 
         votedQuote.Score.Should().Be(quote.Score + 1);
     }
-    
+
     [Fact]
     public async Task Given_NotExistingId_When_VotingForQuote_Then_NotFoundIsReturned()
     {
         var response = await _client.PutAsync($"/quote/{Guid.NewGuid()}/vote", null);
         response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
-    
+
     [Fact]
     public async Task Given_ExistingId_When_ResettingQuoteScore_Then_ScoreIsReset()
     {
         var quote = new Quote("key", "contexnt", 10);
-        
+
         await TestData.InsertRecords(quote);
-        
+
         var response = await _client.PutAsync($"/quote/{quote.Id}/reset", null);
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
@@ -121,7 +118,7 @@ public class ApiTests : IClassFixture<ChuckWebAppFactory>,IAsyncLifetime
 
         votedQuote.Score.Should().Be(0);
     }
-    
+
     [Fact]
     public async Task Given_NotExistingId_When_ResettingQuoteScore_Then_NotFoundIsReturned()
     {
---
+++ Tests/HarvesterTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests;
+
+public class HarvesterTests : IClassFixture<HarvesterFixture>
+{
+    [Fact]
+    public async Task Given_TooLongJokesReturnedByExternalApi_When_HarvestingNewData_Then_TooLongJokesAreFilteredOut()
+    {
+        Assert.True(false);
+    }
+    
+    [Fact]
+    public async Task Given_DuplicatedJokesAmongNewData_When_HarvestingNewData_Then_DuplicatedJokesAreNotSavedButNewOneAre()
+    {
+        Assert.True(false);
+    }
+    
+    [Fact]
+    public async Task Given_InvalidExternalApiResponse_When_HarvestingNewData_Then_HarvesterKeepRunning()
+    {
+        Assert.True(false);
+    }
+
+    [Fact]
+    public async Task Given_ValidDataReturnedByApi_when_HarvestingNewData_Then_ValidDataIsSavedInDatabase()
+    {
+        Assert.True(false);
+    }
+}
